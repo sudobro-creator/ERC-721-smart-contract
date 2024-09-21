5674b3e807cc689d8fe7bb70e7d04dd9400bf73f
sudobro-creator <defcyber@yahoo.com>
2024-09-21 23:21:09 +0100

    updated config

---
+++ scripts/mint.js
@@ -0,0 +1,27 @@
+// Import necessary modules from Hardhat and SwisstronikJS
+const hre = require("hardhat");
+const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
+hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
+async function main() {
+    const replace_contractAddress = "0x1c70aF87ee3d6207bdBe2aAA468b3735c8E265C4";
+    const [from] = await hre.web3.eth.getAccounts();
+    const contractFactory = await hre.ethers.getContractFactory("FreeNFT");
+    const ABI = JSON.parse(contractFactory.interface.formatJson());
+    const contract = new hre.web3.eth.Contract(ABI, replace_contractAddress);
+    const replace_functionArgs = "0x1c70aF87ee3d6207bdBe2aAA468b3735c8E265C4"; // Recipient address
+    console.log("Minting 1 token...");
+    try {
+        const transaction = await contract.methods.safeMint(replace_functionArgs).send({ from });
+        console.log("Transaction submitted! Transaction details:", transaction);
+        // Display success message with recipient address
+        console.log(`Transaction completed successfully! ✅ Non-Fungible Token minted to ${replace_functionArgs}`);
+        console.log("Transaction hash:", transaction.transactionHash);
+    } catch (error) {
+        console.error(`Transaction failed! Could not mint NFT.`);
+        console.error(error);
+    }
+}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
